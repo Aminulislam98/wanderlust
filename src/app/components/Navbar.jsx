ca27fb117179b8f09c2cc299ffffe7d4a67f700d
Aminul islam <aminurrohman98@gmail.com>
2026-05-10 15:00:02 +0100

    added navbar   ,banner , footer

---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,37 @@
+// import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="flex justify-between p-3 max-w-7xl mx-auto w-full">
+      <ul className="flex justify-between gap-3 items-center font-semibold">
+        <li>
+          <Link href={"/"}>Home</Link>
+        </li>
+        <li>
+          <Link href={"/destinations"}>Destinations</Link>
+        </li>
+        <li>
+          <Link href={"/my-bookings"}>My Bookings</Link>
+        </li>
+      </ul>
+      <div>
+        <h1 className="text-[#15A1BF] text-3xl font-bold">Wanderlust</h1>
+      </div>
+      <ul className="flex justify-between gap-3 items-center font-semibold">
+        <li>
+          <Link href={"/profile"}>Profile</Link>
+        </li>
+        <li>
+          <Link href={"/login"}>Login</Link>
+        </li>
+        <li>
+          <Link href={"/signin"}>Sign up</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,9 @@
-import Image from "next/image";
-import { HiH1 } from "react-icons/hi2";
+import Banner from "./components/Banner";
 
 export default function Home() {
-  return <h1>This is home page!</h1>;
+  return (
+    <div>
+      <Banner></Banner>
+    </div>
+  );
 }

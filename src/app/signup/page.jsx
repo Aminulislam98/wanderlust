b82b603ef88cfeb1f19d8b5dee56d137aa5c0912
Aminul islam <aminurrohman98@gmail.com>
2026-05-12 20:50:45 +0100

    install gravity, created signup page and imported ui from hero ui

---
+++ src/app/signup/page.jsx
@@ -0,0 +1,91 @@
+"use client";
+import { Check } from "@gravity-ui/icons";
+import {
+  Button,
+  Description,
+  FieldError,
+  Form,
+  Input,
+  Label,
+  TextField,
+} from "@heroui/react";
+const SignUpPage = () => {
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <div className="max-w-7xl mx-auto w-full py-10">
+      <Form
+        className="flex max-w-xl mx-auto rounded-2xl p-10 flex-col gap-4 border "
+        onSubmit={onSubmit}
+      >
+        <TextField
+          isRequired
+          name="name"
+          type="text"
+          validate={(value) => {
+            if (value < 5) {
+              return "Give at least 3 letters or more!";
+            }
+            return null;
+          }}
+        >
+          <Label>Name</Label>
+          <Input placeholder="Enter Your Name" />
+          <FieldError />
+        </TextField>
+        <TextField
+          isRequired
+          name="email"
+          type="email"
+          validate={(value) => {
+            if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) {
+              return "Please enter a valid email address";
+            }
+            return null;
+          }}
+        >
+          <Label>Email</Label>
+          <Input placeholder="john@example.com" />
+          <FieldError />
+        </TextField>
+        <TextField
+          isRequired
+          minLength={8}
+          name="password"
+          type="password"
+          validate={(value) => {
+            if (value.length < 8) {
+              return "Password must be at least 8 characters";
+            }
+            if (!/[A-Z]/.test(value)) {
+              return "Password must contain at least one uppercase letter";
+            }
+            if (!/[0-9]/.test(value)) {
+              return "Password must contain at least one number";
+            }
+            return null;
+          }}
+        >
+          <Label>Password</Label>
+          <Input placeholder="Enter your password" />
+          <Description>
+            Must be at least 8 characters with 1 uppercase and 1 number
+          </Description>
+          <FieldError />
+        </TextField>
+        <div className="flex gap-2">
+          <Button type="submit">
+            <Check />
+            Submit
+          </Button>
+          <Button type="reset" variant="secondary">
+            Reset
+          </Button>
+        </div>
+      </Form>
+    </div>
+  );
+};
+
+export default SignUpPage;
